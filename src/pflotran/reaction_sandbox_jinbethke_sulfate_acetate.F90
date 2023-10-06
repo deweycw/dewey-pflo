16dc4b266cf474b5fe0b87a97e72f655830ed17d
Christian Dewey <dewey.christian@gmail.com>
2023-10-06 15:59:34 -0500

    restoring old Ft logic

--- src/pflotran/reaction_sandbox_jinbethke_sulfate_acetate.F90
+++ src/pflotran/reaction_sandbox_jinbethke_sulfate_acetate.F90
@@ -297,8 +297,10 @@ subroutine JinBethkeSulfateEvaluate(this, Residual,Jacobian,compute_derivative, 
 
   ! Thermodynamic factor 
   Ft = 1.d0 - exp((dGr + m*dG_ATP) / (chi * RT))
-  Ftr = 0.d0
-  if (Ft > 0.d0) then 
+
+  if (Ft <= 0.d0) then 
+    Ftr = 0.d0
+  else
     Ftr = Ft
   endif
 
