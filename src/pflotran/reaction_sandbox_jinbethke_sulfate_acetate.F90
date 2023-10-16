abf81e52fe462517969f2d95bd1e907528acd525
Christian Dewey <dewey.christian@gmail.com>
2023-10-16 13:15:49 -0500

    updated where sulfate rate is multiplied by L_water

--- src/pflotran/reaction_sandbox_jinbethke_sulfate_acetate.F90
+++ src/pflotran/reaction_sandbox_jinbethke_sulfate_acetate.F90
@@ -313,13 +313,13 @@ subroutine JinBethkeSulfateEvaluate(this, Residual,Jacobian,compute_derivative, 
     ! base rate, mol/sec/m^3 bulk
     ! units on k: mol/sec/mol-bio
 
-    Rate_b = -k_rmax *  Facceptor * Fdonor * Ftr * Sim  
+    Rate_b = -k_rmax *  Facceptor * Fdonor * Ftr * Sim * L_water
  
     Rate_sulf = Rate_b
     
     rt_auxvar%auxiliary_data(iauxiliary) = Rate_sulf
 
-    Rate = Rate_b * L_water ! mol/sec
+    Rate = Rate_b  ! mol/sec
       
     ! species-specifc 
     Rate_Ac = Rate * stoi_ac  
